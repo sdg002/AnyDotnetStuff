e7685fb5682d0b9fd545b25e3d0541fd74213878
saurabh <saurabh_dasgupta@hotmail.com>
2022-02-10 21:32:23 +0000

    bootstrap table, refresh button

--- DemoWebAppWithCiCd/FrontEnd/src/public/scripts/default.html.js
+++ DemoWebAppWithCiCd/FrontEnd/src/public/scripts/default.html.js
@@ -2,11 +2,45 @@ import {WeatherEndpoint} from "./config.js"
 
 
 
-export async function WeatherForeCastClickHandler(){
-    
+export async function FetchWeatherForecastHandler(tableElement){
+    ClearWeatherForecastHandler(tableElement)
+    console.log("fetching weather")
     let response=await fetch(WeatherEndpoint)
     console.log(`Got response ${response.status}`)
     let data=await response.json()
     console.log(`Got response ${data.length}`)
-    document.getElementById("results").innerText=JSON.stringify(data)
+
+    let tbodyElement=tableElement.querySelector("tbody")
+    for (let i in data) {
+        let weather=data[i]
+        let tr=document.createElement("tr")
+
+        let date=CreateTHElement(weather.date)
+        tr.append(date)
+
+        let tempF=CreateTHElement(weather.temperatureF)
+        tr.append(tempF)
+
+        let tempC=CreateTHElement(weather.temperatureC)
+        tr.append(tempC)
+
+        let summary=CreateTHElement(weather.summary)
+        tr.append(summary)
+        tbodyElement.append(tr)
+      }    
+}
+
+function CreateTHElement(text){
+    let th=document.createElement("td")
+    let textElement=document.createTextNode(text)
+    th.append(textElement)
+    return th
+}
+
+export function ClearWeatherForecastHandler(tableElement){
+    let tbodyElement=tableElement.querySelector("tbody")
+    console.log("clear weather")
+    while (tbodyElement.hasChildNodes()) {
+        tbodyElement.removeChild(tbodyElement.childNodes[0]);
+      }
 }
