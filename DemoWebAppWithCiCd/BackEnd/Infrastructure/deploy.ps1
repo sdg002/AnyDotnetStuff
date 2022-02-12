839e8e5d097d9145d26760b7b2e298393ee762fa
saurabh <saurabh_dasgupta@hotmail.com>
2022-02-12 20:21:17 +0000

    Delete DemoWebAppWithCiCd/BackEnd/Infrastructure directory

--- DemoWebAppWithCiCd/BackEnd/Infrastructure/deploy.ps1
+++
@@ -1,8 +0,0 @@
-param([string]$pathtozip)
-Set-StrictMode -Version "latest"
-$ErrorActionPreference="Stop"
-
-
-Write-Host "Going to deploy the web app $pathtozip, "
-Write-Host ("Value of environment={0}" -f $env:environment)
-
