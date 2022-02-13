9afafcefe80d70c3b156a3e3989bc7f799841741
saurabh <saurabh_dasgupta@hotmail.com>
2022-02-13 16:18:49 +0000

    Get environment from hostname

--- DemoWebAppWithCiCd/FrontEnd/src/public/scripts/config.js
+++ DemoWebAppWithCiCd/FrontEnd/src/public/scripts/config.js
@@ -1,2 +1,16 @@
-export const ProdServerUrl="https://mydemowebapi123.azurewebsites.net"
+function GetEnvironmentFromUrl()
+{
+    let hostname=window.location.hostname;
+    if (hostname === "localhost"){
+        return "dev";
+    }
+    let frags=hostname.split("-");
+    if (frags.length <2){
+        throw `could not parse the host ${hostname} name to get environment`
+    }
+    return frags[1];
+}
+
+export const environment=GetEnvironmentFromUrl()
+export const ProdServerUrl=`https://mydemowebapi123-${environment}.azurewebsites.net`
 export const WeatherEndpoint=`${ProdServerUrl}/WeatherForecast`;
