945adf83431a98b81e5ec1ee197f5ed98f2e996a
saurabh <saurabh_dasgupta@hotmail.com>
2022-02-13 23:01:34 +0000

    Some more back end unit tests

--- DemoWebAppWithCiCd/BackEnd/src/WebApplication1/Controllers/WeatherForecastController.cs
+++ DemoWebAppWithCiCd/BackEnd/src/WebApplication1/Controllers/WeatherForecastController.cs
@@ -30,7 +30,7 @@ namespace WebApplication1.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(2, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
