a444e13aef33466e554473566ec6fddf047820b6
saurabh <saurabh_dasgupta@hotmail.com>
2022-02-17 12:04:05 +0000

    Added logging

--- DemoWebAppWithCiCd/BackEnd/src/WebApplication1/Controllers/WeatherForecastController.cs
+++ DemoWebAppWithCiCd/BackEnd/src/WebApplication1/Controllers/WeatherForecastController.cs
@@ -30,13 +30,16 @@ namespace WebApplication1.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var weatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+
+            _logger.LogInformation($"Found {weatherForecast.Length} weather information records");
+            return weatherForecast;
         }
     }
 }
