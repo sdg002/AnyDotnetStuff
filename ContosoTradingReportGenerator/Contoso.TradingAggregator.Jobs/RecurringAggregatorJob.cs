6773245ee7ce9c6900452de6f33c1e2fe711a5b4
saurabh <saurabh_dasgupta@hotmail.com>
2022-03-06 16:39:37 +0000

    Polly

--- ContosoTradingReportGenerator/Contoso.TradingAggregator.Jobs/RecurringAggregatorJob.cs
+++ ContosoTradingReportGenerator/Contoso.TradingAggregator.Jobs/RecurringAggregatorJob.cs
@@ -3,6 +3,8 @@ using Contoso.TradingAggregator.Domain.extensions;
 using Contoso.TradingAggregator.Domain.infrastructure;
 using Contoso.TradingAggregator.Domain.interfaces;
 using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,7 @@ namespace Contoso.TradingAggregator.Jobs
 {
     public class RecurringAggregatorJob : Microsoft.Extensions.Hosting.BackgroundService
     {
+        public const int RetryAttempts = 3;
         private const int ReportExecutionTimingThresholdSeconds = 60;
         private readonly IClock _clock;
         private readonly ICsvGenerator _csvGenerator;
@@ -33,14 +36,11 @@ namespace Contoso.TradingAggregator.Jobs
             this._reportsRepo = reportsRepo;
             this._tradingConnector = tradingConnector;
             this._clock = clock;
-            this.MaximumDelayBetweenConsecutiveReportExtractions = config.MaximumSecondsDelayBetweenConsecutiveReporExtrations;
+            this.MaximumSecondsDelayBetweenConsecutiveReportExtractions = config.MaximumSecondsDelayBetweenConsecutiveReporExtrations;
             this._csvGenerator = csvGenerator;
-
-            this.DelayBetweenConsecutiveAttempts = config.SecondsDelayBetweenConsecutiveAttempts;
         }
 
-        private int DelayBetweenConsecutiveAttempts { get; set; }
-        private int MaximumDelayBetweenConsecutiveReportExtractions { get; set; }
+        private int MaximumSecondsDelayBetweenConsecutiveReportExtractions { get; set; }
 
         public List<AggregatedTradePosition> CalculateAggregatedTrades(List<TradePosition> trades)
         {
@@ -49,35 +49,55 @@ namespace Contoso.TradingAggregator.Jobs
             return aggregatedPositions.OrderBy(ag => ag.Period).ToList();
         }
 
-        public async Task ExecuteEarliestReportGenerationAsync()
+        /// <summary>
+        /// Extracts the earlies pending report
+        /// </summary>
+        /// <returns>The count of pending reports, i.e. reports should have been already generated at the given clock time</returns>
+        public async Task<int> ExecuteEarliestReportGenerationAsync()
         {
-            _logger.LogInformation($"Executing job {_clock.GetCurrentTime()}");
-
             DateTime currentClockTime = _clock.GetCurrentTime();
             DateTime startOfReportingDay = GetStartOfReportingDay();
             DateTime endOfReportingDay = GetEndOfReportingDay();
 
-            var alreadyGeneratedReports = (await _reportsRepo.GetReports())
+            _logger.LogInformation($"Executing job {currentClockTime}, startOfReportingDay={startOfReportingDay} , endOfReportingDay={endOfReportingDay}");
+
+            AsyncRetryPolicy retryPolicy = CreateExponentialBackoffPolicy();
+
+            var allExistingReports = await retryPolicy
+                .ExecuteAsync<List<AggregatedReportBase>>(() => _reportsRepo.GetReports());
+
+            var alreadyGeneratedReports = allExistingReports
                 .Where(rpt => rpt.ReportDate > startOfReportingDay)
                 .OrderBy(rpt => rpt.ReportDate)
                 .ToList();
 
-            _logger.LogInformation($"Found {alreadyGeneratedReports.Count} reports in the repository. Most recent being generated at {alreadyGeneratedReports.LastOrDefault()?.ReportDate}");
+            _logger
+                .LogInformation($"Found {alreadyGeneratedReports.Count} reports in the repository. Most recent being generated at '{alreadyGeneratedReports.LastOrDefault()?.ReportDate}'");
 
-            List<DateTime> desiredReportExecutionTimings = GenerateTableOfDesiredReportExecutionTimings(startOfReportingDay, endOfReportingDay);
+            List<DateTime> desiredReportExecutionTimings =
+                GenerateTableOfDesiredReportExecutionTimings(startOfReportingDay, endOfReportingDay);
 
-            var firstMissingExecutionTiming = desiredReportExecutionTimings
+            var allReportsWhichHaveMissedOrNotYetGenerated = desiredReportExecutionTimings
                 .SkipWhile(d => IsDesiredExecutionTimingAlreadyGenerated(d, alreadyGeneratedReports))
-                .FirstOrDefault();
+                .ToList();
 
-            if (firstMissingExecutionTiming >= currentClockTime)
+            var allReportsWhichAreAlreadyDue = allReportsWhichHaveMissedOrNotYetGenerated
+                .Where(d => d <= currentClockTime).ToList();
+
+            if (allReportsWhichAreAlreadyDue.Count == 0)
             {
                 _logger.LogInformation($"All reports generated for the given reporting date. Current time:{_clock.GetCurrentTime()}");
-                return;
+                return 0;
             }
 
+            _logger.LogInformation($"There are {allReportsWhichAreAlreadyDue.Count} reports which are already due for generation");
+            var firstMissingExecutionTiming = allReportsWhichAreAlreadyDue.First();
+
             _logger.LogInformation($"Going to fetch all trades for the date: {firstMissingExecutionTiming}");
-            var trades = await _tradingConnector.GetTradesAsync(firstMissingExecutionTiming);
+
+            var trades = await retryPolicy
+                .ExecuteAsync<List<TradePosition>>(() => _tradingConnector.GetTradesAsync(firstMissingExecutionTiming));
+
             _logger.LogInformation($"Found {trades.Count} trades for the date: {firstMissingExecutionTiming}");
 
             List<AggregatedTradePosition> aggregatedPositions = CalculateAggregatedTrades(trades);
@@ -85,17 +105,56 @@ namespace Contoso.TradingAggregator.Jobs
             string csvContents = CreateAggregatedCsvReport(aggregatedPositions);
 
             _logger.LogInformation($"The report generated at time {firstMissingExecutionTiming} will be saved");
-            await _reportsRepo.SaveReport(firstMissingExecutionTiming, csvContents);
+
+            await retryPolicy.ExecuteAsync(() => _reportsRepo.SaveReport(firstMissingExecutionTiming, csvContents));
+
+            return (allReportsWhichAreAlreadyDue.Count - 1);
+        }
+
+        public DateTime GetEndOfReportingDay()
+        {
+            return GetStartOfReportingDay().AddDays(+1).Date.AddHours(22).AddMinutes(59);
+        }
+
+        public DateTime GetStartOfReportingDay()
+        {
+            var currentDateTime = _clock.GetCurrentTime();
+            return (currentDateTime.Hour >= 23) ? (currentDateTime.Date.AddHours(23)) : (currentDateTime.Date.AddDays(-1).AddHours(23));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                await ExecuteEarliestReportGenerationAsync();
-                _logger.LogInformation($"Waiting for execution, {this.DelayBetweenConsecutiveAttempts} seconds");
-                await Task.Delay(this.DelayBetweenConsecutiveAttempts * 1000, stoppingToken);
+                try
+                {
+                    int pendingReports = await ExecuteEarliestReportGenerationAsync();
+                    if (pendingReports == 0)
+                    {
+                        var delayMilliseconds = this.MaximumSecondsDelayBetweenConsecutiveReportExtractions * 1000;
+                        _logger.LogInformation($"No pending report. Waiting for execution, {delayMilliseconds} ms");
+                        await Task.Delay(delayMilliseconds, stoppingToken);
+                    }
+                    else
+                    {
+                        _logger.LogInformation($"{pendingReports} pending reports in the queue. Not going to wait ");
+                        await Task.Delay(0, stoppingToken);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Exception while calling method {nameof(ExecuteEarliestReportGenerationAsync)}");
+                }
             }
+        }
+
+        private static AsyncRetryPolicy CreateExponentialBackoffPolicy()
+        {
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(
+                RetryAttempts,
+                attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)));
         }
 
         private bool ApproximateDateTimeMatch(DateTime dateTime1, DateTime dateTime2)
@@ -110,18 +169,18 @@ namespace Contoso.TradingAggregator.Jobs
 
         private List<DateTime> GenerateTableOfDesiredReportExecutionTimings(DateTime startOfReportingDay, DateTime endOfReportingDay)
         {
-            _logger.LogInformation($"Generating table of expected report timings using the Max permitted delay between consecutive reports:{MaximumDelayBetweenConsecutiveReportExtractions}");
+            _logger.LogInformation($"Generating table of expected report timings using the Max permitted delay between consecutive reports:{MaximumSecondsDelayBetweenConsecutiveReportExtractions} seconds");
             var desiredReportExecutionTimings = new List<DateTime>();
 
             while (true)
             {
                 if (!desiredReportExecutionTimings.Any())
                 {
-                    desiredReportExecutionTimings.Add(startOfReportingDay.AddSeconds(MaximumDelayBetweenConsecutiveReportExtractions));
+                    desiredReportExecutionTimings.Add(startOfReportingDay.AddSeconds(MaximumSecondsDelayBetweenConsecutiveReportExtractions));
                 }
                 else
                 {
-                    desiredReportExecutionTimings.Add(desiredReportExecutionTimings.Last().AddSeconds(MaximumDelayBetweenConsecutiveReportExtractions));
+                    desiredReportExecutionTimings.Add(desiredReportExecutionTimings.Last().AddSeconds(MaximumSecondsDelayBetweenConsecutiveReportExtractions));
                 }
 
                 if (desiredReportExecutionTimings.Last() >= endOfReportingDay)
@@ -129,19 +188,10 @@ namespace Contoso.TradingAggregator.Jobs
                     break;
                 }
             }
-            desiredReportExecutionTimings.ForEach(timing => _logger.LogInformation($"{timing}"));
+            _logger.LogInformation($"Generated {desiredReportExecutionTimings.Count} execution timings");
+            _logger.LogInformation($"First execution time in the current reporting day:{desiredReportExecutionTimings.First()}");
+            _logger.LogInformation($"Last execution time in the current reporting day:{desiredReportExecutionTimings.Last()}");
             return desiredReportExecutionTimings;
-        }
-
-        private DateTime GetEndOfReportingDay()
-        {
-            return GetStartOfReportingDay().AddDays(+1).Date.AddHours(23).AddMinutes(59);
-        }
-
-        private DateTime GetStartOfReportingDay()
-        {
-            var currentDateTime = _clock.GetCurrentTime();
-            return (currentDateTime.Hour >= 23) ? (currentDateTime.Date.AddHours(23)) : (currentDateTime.Date.AddDays(-1).AddHours(23));
         }
 
         /// <summary>
