5eef699d37b13191e7fbc4bc9162e49c8ce06568
saurabh <saurabh_dasgupta@hotmail.com>
2022-03-24 21:58:44 +0000

    Mocking HTTP client clas

--- WebsiteCrawler/WebsiteCrawler.Executable/CmdLineArgumentModel.cs
+++ WebsiteCrawler/WebsiteCrawler.Executable/CmdLineArgumentModel.cs
@@ -7,10 +7,10 @@ namespace WebsiteCrawler.Executable
 {
     public class CmdLineArgumentModel
     {
-        [Option("maxsites", Required = true, HelpText = "An upper limit on the number of sites to search. Example: 30")]
+        [Option("maxsites", Required = true, HelpText = "An upper limit on the number of sub-sites to search.Example:30. This number is used as a stopping criteria which is very useful for deeply nested web sites.")]
         public int MaxSites { get; set; }
 
-        [Option("url", Required = true, HelpText = "The URL of the site to search")]
+        [Option("url", Required = true, HelpText = "The URL of the site to search. Example: https://www.cnn.com")]
         public string Url { get; set; }
     }
 }
