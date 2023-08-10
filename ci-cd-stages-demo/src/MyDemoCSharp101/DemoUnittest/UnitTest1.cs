a60dbe76a0aa4a817bfc60af39ddc6efad50554e
saurabh <saurabh_dasgupta@hotmail.com>
2023-08-10 18:39:04 +0100

    unit test

---
+++ ci-cd-stages-demo/src/MyDemoCSharp101/DemoUnittest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace DemoUnittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
