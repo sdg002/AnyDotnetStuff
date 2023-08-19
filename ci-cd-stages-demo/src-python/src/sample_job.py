844192f03d624412a24f7ebb4471c0ca335b55f3
saurabh <saurabh_dasgupta@hotmail.com>
2023-08-20 00:41:10 +0100

    added second job and display all jobs

--- ci-cd-stages-demo/src-python/src/sample_job.py
+++ ci-cd-stages-demo/src-python/src/sample_job.py
@@ -3,10 +3,17 @@ import datetime
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job(trigger='cron', id='my_job_id', second=30)
-def some_decorated_task():
-    print(f"Inside function some_decorated_task  current time is:{datetime.datetime.now()}")
+@sched.scheduled_job(trigger='cron', id='my_job_id_1', second=30)
+def some_decorated_task_1():
+    print(f"Inside first_job  current time is:{datetime.datetime.now()}")
+
+@sched.scheduled_job(trigger='cron', id='my_job_id_2', second=45)
+def some_decorated_task_2():
+    print(f"Inside second_job  current time is:{datetime.datetime.now()}")
 
 print("Hello world - this is an example of scheduled jobs via AP Scheduler")
+all_jobs = sched.get_jobs()
+print(f"Displaying all registered jobs:")
+print(all_jobs)
 sched.start()
     
